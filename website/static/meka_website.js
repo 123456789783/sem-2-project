a0e33a2dbdbb183af9e4ece8711501b62682dca6
123456789783 <1119112@apps.nsd.org>
2025-04-30 09:36:33 -0700

    Add files via upload

--- website/static/meka_website.js
+++ website/static/meka_website.js
@@ -3,3 +3,11 @@ const hamMenu = document.querySelector('.ham-menu');
 hamMenu.addEventListener('click',() => {
     offScreenMenu.classList.toggle('show');
 }) 
+
+
+setTimeout(() => {
+    const overlay = document.getElementById('overlay-container');
+     if (overlay) {
+        overlay.classList.remove('active');
+    }
+}, 5000);
